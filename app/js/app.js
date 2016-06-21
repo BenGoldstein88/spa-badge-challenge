46ca0983a0c882412f91442d832b405cdb512137
Ben Goldstein & Robin Soubry & Shin <pair+bengoldstein88.robinsoubry.shinwang1@devbootcamp.com>
2016-06-21 15:54:34 -0700

    create handlebar template for teacher list, implement w ajax

--- app/js/app.js
+++ app/js/app.js
@@ -10,12 +10,29 @@ $.ready( function() {
       var teacherString = "<ul>"
       var teachersListJSON = response.currentTarget.response
       var teachersList = JSON.parse(teachersListJSON)
-      for (i = 0; i < teachersList.length; i++){
-        teacherString += "<li><a href='teachers/" + teachersList[i].id+ "'>" + teachersList[i].name + "</a></li>"
-      }
-      teacherString += "</ul>"
-      $("#teachers_list").innerHTML = teacherString;
+
+      var target = $("#teacher-list");
+      // var wrap = document.createElement('div')
+      // wrap.appendChild(target.cloneNode(true));
+      var source = target.innerHTML;
+      var template = Handlebars.compile(source);
+      var context = {teachers: teachersList}
+      Handlebars.registerHelper('list', function(items, options) {
+        var out = "<ul>";
+
+        for(var i=0, l=items.length; i<l; i++) {
+          out = out + "<li>" + options.fn(items[i]) + "</li>";
+        }
+
+        return out + "</ul>";
+      });
+      var html = template(context);
+      $("#teachers-list").innerHTML = html;
+
     },
     fail: function (response) {console.log('fail' + response)}
   });
+
+// Hide teacher index
+
 })
