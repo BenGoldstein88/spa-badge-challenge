8b1ce7ed4fe944e882a5953c525201fe93fdf1fe
Ben Goldstein & Robin Soubry & Shin <pair+bengoldstein88.robinsoubry.shinwang1@devbootcamp.com>
2016-06-21 17:46:28 -0700

    Add show teacher badges feature

--- app/js/app.js
+++ app/js/app.js
@@ -1,59 +1,55 @@
 var showTeacherHeader = function(teacher){
-  console.log(teacher);
-
   var target = $("#teacher-header-script");
   var source = target.innerHTML;
   var template = Handlebars.compile(source);
   var context = {teacher: teacher};
-
   var html = template(context);
-
   $("#teacher-header").innerHTML = html;
-
-
 }
 
-// var showTeacherBadges = function(){
-
-// }
+var showTeacherBadges = function(teacher){
+  var target = $("#teacher-badges-script");
+  var source = target.innerHTML;
+  var template = Handlebars.compile(source);
+  var context = {badges: teacher.badges}
+  Handlebars.registerHelper('orderedList', function(items, options) {
+    var out = "<ol>";
+    for(var i=0, l=items.length; i<l; i++) {
+      out = out + "<li>" + options.fn(items[i]) + "</li>";
+    }
+    return out + "</ol>";
+  });
+  var html = template(context);
+  $("#teacher-badges").innerHTML = html;
+}
 
 var clickTeacherHandler = function(){
   $('.teacher').on('click', function(event){
     event.preventDefault();
-    console.log(this.href);
-    // debugger;
     $.ajax({
       url: this.href ,
       type: 'get',
       success: function(response){
-        // console.log(response);
         var teacher = JSON.parse(response.currentTarget.response);
         $('#teachers-list').hide();
         showTeacherHeader(teacher);
-
-        // showTeacherBadges();
+        showTeacherBadges(teacher);
       },
       fail: function (response) {console.log('fail' + response)}
     })
-
   })
 }
 
 $.ready( function() {
   // Do Ajax call to spa badge
   // Parse return JSON object and print it as list items
-
   // Teachers-list
   $.ajax({
     url: "http://spa-badge-api.herokuapp.com/teachers",
     type: "GET",
     success: function(response) {
-      var teacherString = "<ul>"
       var teachersListJSON = response.currentTarget.response
-      console.log(teachersListJSON);
-      console.log("$$$$$")
       var teachersList = JSON.parse(teachersListJSON)
-      console.log(teachersList);
       var target = $("#teacher-list");
       var source = target.innerHTML;
       var template = Handlebars.compile(source);
@@ -64,12 +60,10 @@ $.ready( function() {
         for(var i=0, l=items.length; i<l; i++) {
           out = out + "<li>" + options.fn(items[i]) + "</li>";
         }
-
         return out + "</ul>";
       });
       var html = template(context);
       $("#teachers-list").innerHTML = html;
-
       clickTeacherHandler();
     },
     fail: function (response) {console.log('fail' + response)}
